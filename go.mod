9f0adc5f194b67619d4f4772dd62e0aa6f2df72c
phy <cc_@live.com>
2019-10-26 11:26:39 +0800

    0

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module gin
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
+	github.com/googollee/go-engine.io v1.4.2
+	github.com/googollee/go-socket.io v1.4.2
 	github.com/jinzhu/gorm v1.9.11
 	github.com/sirupsen/logrus v1.4.2
 )
--- route/route.go
+++ route/route.go
@@ -3,7 +3,9 @@ package route
 import (
 	"gin/controller"
 	"gin/middleware"
+	"time"
 
+	// "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,9 +15,15 @@ var (
 
 func BuildRouter() *gin.Engine {
 	router := gin.New()
-	router.Use(middleware.Cros, gin.Logger())
-	v1 := router.Group("api/v1")
+	router.Use(gin.Logger(), middleware.Cros)
+	v1 := router.Group("/api/v1")
 	{
+		v1.GET("/alive", func(c *gin.Context) {
+			c.JSON(200, map[string]interface{}{
+				"message": "server alive",
+				"time":    time.Now(),
+			})
+		})
 		v1.POST("/login", userCtrl.Login)
 		v1.POST("/github_oauth")
 		v1.POST("/register", userCtrl.Register)
