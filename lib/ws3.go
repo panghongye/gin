9b696eb3e19142206020a9078cd7b135d1b99057
phy <cc_@live.com>
2019-11-13 18:47:22 +0800

    0

--- lib/ws3.go
+++ lib/ws3.go
@@ -3,7 +3,6 @@ package lib
 import (
 	"gin/socketio"
 	"log"
-	"strings"
 	"time"
 	//  "github.com/zyxar/socketio"
 )
@@ -21,37 +20,14 @@ func GetWs3() *socketio.Server {
 		so.Close()
 	})
 	// assets
-	{
-		// sp.OnConnect(func(so socketio.Socket) {
-		// 	so.Join("a")
-		// 	log.Println("OnConnect <<", so.Sid())
-		// }).
-		// 	OnEvent("chat message", func(so socketio.Socket, data string) {
-		// 		log.Println(data)
-		// 		sp.BroadcastToRoom("a", "chat message", data)
-		// 	})
-	}
-
-	sp.OnEvent("initSocket", func(s socketio.Socket, userID int) {
-		t := userService.GetByID(userID)
-		socketId := s.Sid()
-		if t.Socketid != "" {
-			socketId = strings.Split(t.Socketid, ",")[0] + "," + socketId
-		}
-		if result := userService.SaveUserSocketId(userID, socketId); result.Error != nil {
-			s.Emit("error", struct {
-				Code    int
-				Message string
-			}{
-				500,
-				result.Error.Error(),
-			})
-			return
-		}
-		s.Emit("initSocket success")
-	}).OnEvent("initGroupChat", func(s socketio.Socket, userID int) {
-		s.Emit("initGroupChat success")
-	})
+	sp.OnConnect(func(so socketio.Socket) {
+		so.Join("a")
+		log.Println("OnConnect <<", so.Sid())
+	}).
+		OnEvent("chat message", func(so socketio.Socket, data string) {
+			log.Println(data)
+			sp.BroadcastToRoom("a", "chat message", data)
+		})
 
 	return server
 }
